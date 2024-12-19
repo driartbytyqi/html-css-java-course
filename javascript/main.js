beaa61ef7293de44930f8eb49492c183103018a9
driartbytyqi <driartbytyqii@gmail.com>
2024-12-19 07:10:13 -0800

    ndryshim

--- javascript/main.js
+++ javascript/main.js
@@ -1,16 +1,40 @@
-console.log('hello woarld')
- var b = 20;
- var c = 30;
- var D = b * c;
- var d = null;
+// console.log('hello woarld')
+//  var b = 20;
+//  var c = 30;
+//  var D = b * c;
+//  var d = null;
 
- var n = 99;
- n++;
- console.log(n);
-
-
- console.log(n);
+//  var n = 99;
+//  n++;
+//  console.log(n);
 
 
- var a = 50 +100;
- console.log(a);
+//  console.log(n);
+
+
+//  var a = 50 +100;
+//  console.log(a);
+var v=5;
+var w=5;
+console.log(v==w);
+
+var x=60;
+var y="60";
+console.log(x===y);
+
+var num1=100;
+var num2=101;
+console.log(num1 >= num2);
+var firstName='arion';
+var lastName='morina';
+var fullName=firstName+" "+lastName;
+console.log(fullName);
+
+var age=23;
+console.log(age >=18 && age<=65);
+var var1=15;
+var var2=60;
+console.log(var1==15||var2 ==60);
+var var3=9;
+var var4=10;
+console.log(!(var3==var4));
