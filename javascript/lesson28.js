143af2b7294e8427caef5d1e5108cb68542fceec
driartbytyqi <driartbytyqii@gmail.com>
2025-01-09 16:16:42 +0100

    ndryshim

---
+++ javascript/lesson28.js
@@ -0,0 +1,24 @@
+//var button1= document.querySelector("button")
+
+//button1.addEventListener('click',function(){
+  //  alert('hello world');
+//})
+
+// var button2 = document.getElementById('btn1')
+var button3 = document.getElementById('btn2');
+var button4 = document.getElementById('btn3');
+var v_button = document.getElementById('button1');
+var v_text = document.getElementById('txt1');
+
+v_button.onclick = function(){
+    v_text.style.color
+}
+
+button2.onmouseover = function(){
+    alert('over')
+}button3.onmouseover = function(){
+    alert('leave')
+}
+button4.onmouseover = function(){
+    alert('wheel')
+}
