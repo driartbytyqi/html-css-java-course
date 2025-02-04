7d82369cb7e55a8d9e3f7478a6c81f63e0bd5e5d
driartbytyqi <driartbytyqii@gmail.com>
2025-02-04 07:14:44 -0800

    ndryshim

---
+++ javascript/lesson33.js
@@ -0,0 +1,25 @@
+function validation(){
+    var name = document.getElementById('name').value;
+    var age= document.getElementById('age').value;
+    var valid_name= /^[A-Za-z]+$/;
+    var valid_age= /^[0-9]+$/;
+    // if(name.match(valid_name)){
+    //     return true;
+    // }else{
+    //     document.getElementById('name_error').style.visibility='visible';
+    //     document.getElementById()
+    // }
+
+
+    if(name.match(valid_name) && age.match(valid_age)){
+        return true;
+}else if(!(name.match(valid_name))){
+    document.getElementById('name_error').style.visibility='visible';
+    document.getElementById('name').style.borderColor = 'red';
+    return false;
+}else if(!(age.match(valid_age))){
+    document.getElementById('age_error').style.visibility='visible';
+    document.getElementById('age').style.borderColor = 'red';
+    return false;
+}
+}
